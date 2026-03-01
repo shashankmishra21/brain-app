e2dec0e4aee1520368ddfd5279473e465b40a3a0
Shashank Mishra <mishrashashank2106@gmail.com>
2026-03-01 15:07:44 +0530

    backend url update

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const JWT_SECRET = "shashankmishraforbrainapp";
 
-//export const BACKEND_URL = "https://linkify-kysf.onrender.com";
+export const BACKEND_URL = "https://linkify-kysf.onrender.com";
 
 
-export const BACKEND_URL = "http://localhost:3000";
+// export const BACKEND_URL = "http://localhost:3000";
